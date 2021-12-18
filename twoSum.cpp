9ab556ee9d7e64ccd0bde8ad0423ef47230597d2
Ali Zain Charolia <63433077+alizain-c@users.noreply.github.com>
2021-12-17 22:58:19 -0600

    Create twoSum.cpp
    LC problem Solution [O(n^2)]

---
+++ twoSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> indexes; 
+        for(int i = 0; i < nums.size(); i++) {
+          for(int j = i; j < nums.size(); j++) {
+            if((nums[i] + nums[j]) == target && i != j) {
+              indexes.push_back(i);
+              indexes.push_back(j);
+              break; 
+            }
+          }
+        }  
+    return indexes; 
+    }
+};
